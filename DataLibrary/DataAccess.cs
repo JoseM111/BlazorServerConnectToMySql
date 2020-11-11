8c0587fec9b6a13af54e709df4e02afa93d723d5
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2020-11-11 20:40:56 +0100

    Update DataAccess.cs

--- DataLibrary/DataAccess.cs
+++ DataLibrary/DataAccess.cs
@@ -12,11 +12,13 @@ namespace DataLibrary
         public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStr) {
             /* • Connects with our connection string, even if it
                  crashes the connection will close properly everytime */
+            
             using IDbConnection connection = new MySqlConnection(connectionStr);
             /* • Rows of type T(Generic|Any)  we do in await to let the conversion
                  from type T to the type being returned & then return it as a List()*/
             // ReSharper disable once SuggestVarOrType_Elsewhere
             IEnumerable<T> rows = await connection.QueryAsync<T>(sql: sql, param: parameters);
+            
             return rows.ToList();
         }
         
@@ -24,6 +26,7 @@ namespace DataLibrary
             /* • Connects with our connection string, even if it
                  crashes the connection will close properly everytime */
             using IDbConnection connection = new MySqlConnection(connectionStr);
+            
             /* • Rows of type T(Generic|Any) Returning the Task */
             // ReSharper disable once SuggestVarOrType_Elsewhere
             return connection.ExecuteAsync(sql: sql, param: parameters);
